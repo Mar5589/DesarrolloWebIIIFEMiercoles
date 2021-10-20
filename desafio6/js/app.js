76e5b774fecda78390f23c2d7fd155cf86f80124
Mar5589 <marcos.ch.m.12@gmail.com>
2021-10-20 20:00:32 -0300

    clase de repaso

--- desafio6/js/app.js
+++ desafio6/js/app.js
@@ -1,22 +1,25 @@
-let miDiv = document.querySelector("#principal")
-let pokemones = [];
-const cargarlistado=()=>{
+let miDiv = document.getElementById("principal");
+let pokemones=[];
+const cargarListado=()=>{
     let url="https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
     fetch(url)
     .then((response)=>{
         return response.json();
-
     })
     .then((objetoJson)=>{
-        const pokms =objetoJson.results;
-        console.log(pokms)
-        pokemones=pokms
-            pokms.forEach((p)=>{
-                miDiv.innerHTML+=` 
-                <button class="btn btn-primary">${p.name}</button>`;
-        })
-       
+        const pokemons = objetoJson.results;
+        console.log(pokemons)
+        pokemones=pokemons
+        pokemons.forEach((p)=> {
+            miDiv.innerHTML+=`
+            <button class='btn btn-primary' onclick="crearImagen('${p.url}')"> ${p.name}</button>`;
+        //    `<button class="btn btn-primary"> ${p.name} </button>`
+ 
+        });
     })
-};
-//alert();
-cargarlistado();
+}
+cargarListado();
+const crearImagen=()=>{
+    let contenedor = document.getElementById("contenedor");
+alert(url)
+}
